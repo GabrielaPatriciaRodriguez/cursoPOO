ecedff732faeed478b5af10bb55849c86000cb06
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2022-02-11 18:55:05 -0300

    mas practicas

--- main.js
+++ main.js
@@ -95,17 +95,22 @@ class Cursos {
         clases = [],
         comentarios,
         preguntas,
+        isFree = false,
+        lang = "spanish",
     }){
         this.name = name;
         this.clases = clases;
         this.comentarios = comentarios;
         this.preguntas = preguntas;
+        this.isFree = isFree;
+        this.lang = lang;
     }
 }
 
 const cursoBasicoDeProgramacion = new Cursos({
     name: "Curso Basico de Programacion",
     clases: [],
+    isFree: true,
 })
 
 const cursoPracticoDeProgramacion = new Cursos({
@@ -116,6 +121,7 @@ const cursoPracticoDeProgramacion = new Cursos({
 const cursoBasicoDeHtml = new Cursos({
     name: "Curso Basico de HTML",
     clases: [],
+    lang: "english",
 })
 
 //Prototipo Rutas de Aprendizaje
@@ -146,3 +152,153 @@ const escuelaDeData = new RutasDeAprendizaje({
 
 //Encapsulamiento con get y set es para protegernos de errores que puedan tener
 //nuestros usuarios
+
+
+
+//Creando una super clase, que sea la clase madre de la cual derivaran las subclases
+
+class EstudianteMama {
+    constructor({
+        name,
+        username,
+        age,
+        email,
+        facebook = undefined,
+        twiter = undefined,
+        instagram = undefined,
+        cursesAproved = [],
+        rutasDeAprendizaje = [],  
+    }){
+        this.name = name;
+        this.username = username;
+        this.age = age;
+        this.email = email;
+        this.socialMedia = {
+            facebook,
+            twiter,
+            instagram
+        }
+        this.cursesAproved = cursesAproved;
+        this.rutasDeAprendizaje = rutasDeAprendizaje;
+    }
+    publicarComentario(contenidoDelComentario){
+        //crear una instancia de la clase comentario
+        const comentario = new Comentario({
+            comentario: contenidoDelComentario,
+            nombreDelQueComenta: this.name,
+        });
+        comentario.publicar();
+    }
+}
+
+// Subclases que heredaran de la clase madre con la palabra reservada extend
+//Extiendo todo, hereda, de la clase madre 
+//con la palabra super llamamos al constructor de nuestra clase madre
+
+class EstudiantesFree extends EstudianteMama{
+    constructor(props){
+        super(props);
+    }
+    aprobarCurso(nuevoCurso){
+        if(nuevoCurso.isFree === true){
+            this.cursesAproved.push(nuevoCurso);
+        } else{
+            console.warn("Lo siento " + this.name + " solo puedes tomar cursos gratis...")
+        }
+    }
+}
+
+class EstudiantesBasic extends EstudianteMama{
+    constructor(props){
+        super(props);
+    }
+    aprobarCurso(nuevoCurso){
+        if(nuevoCurso.lang !== "english"){
+            this.cursesAproved.push(nuevoCurso);
+        } else{
+            console.warn("Lo siento " + this.name + " no puedes tomar cursos en ingles...")
+        }
+    }
+}
+
+class EstudiantesExpert extends EstudianteMama{
+    constructor(props){
+        super(props);
+    }
+    aprobarCurso(nuevoCurso){
+        this.cursesAproved.push(nuevoCurso);
+    }
+}
+
+class EstudiantesTeacher extends EstudianteMama{
+    constructor(props){
+        super(props);
+    }
+    aprobarCurso(nuevoCurso){
+        this.cursesAproved.push(nuevoCurso);
+    }
+    publicarComentario(contenidoDelComentario){
+        //crear una instancia de la clase comentario
+        const comentario = new Comentario({
+            comentario: contenidoDelComentario,
+            nombreDelQueComenta: this.name,
+            rolDelQueComenta: "profesor",
+        });
+        comentario.publicar();
+    }
+}
+//Ahora nuestros estudiantes seran instancias de las subclases pero heredaran la superclase
+const miguelito2 = new EstudiantesFree(
+    {
+        name: "Miguelito",
+        username: "miguelitoFeliz",
+        age: 18,
+        email: "miguelito@gmail.com",
+        cursesAproved: [],
+    }
+);
+
+const patricia = new EstudiantesBasic(
+    {
+        name: "Patricia",
+        username: "gabypat",
+        age: 38,
+        email: "gaby@gmail.com"
+    }
+);
+
+const mauri = new EstudiantesExpert(
+    {
+        name: "Mauricio",
+        username: "mauOasis",
+        age: 28,
+        email: "mau@gmail.com"
+    }
+);
+
+//Polimorfismo: Permite cambiar los metodos que hereda de la clase madre.
+
+class Comentario {
+    constructor({
+        comentario,
+        nombreDelQueComenta,
+        rolDelQueComenta = "estudiante",
+    }){
+        this.comentario = comentario;
+        this.nombreDelQueComenta = nombreDelQueComenta;
+        this.rolDelQueComenta = rolDelQueComenta;
+    }
+    publicar(){
+        console.log(this.nombreDelQueComenta + " (" + this.rolDelQueComenta + ")");
+        console.log(this.comentario);
+    }
+}
+
+const freddy = new EstudiantesTeacher(
+    {
+        name: "Freddy",
+        username: "freddyVega",
+        age: 38,
+        email: "freddier@gmail.com",
+    }
+);
